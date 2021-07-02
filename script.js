0bfe25a1de8b1e219f5a716d9a49edb703425fd4
hachi-ops <elen20@protonmail.com>
2021-07-02 21:36:24 +0100

    form validation

--- script.js
+++ script.js
@@ -26,3 +26,49 @@
 // function myFunction() {
 //     document.querySelector(".error-text").innerHTML = "Hello World";
 //   }
+
+
+
+// Defining a function to display error message
+function printError(elemId, hintMsg) {
+    document.getElementById(elemId).innerHTML = hintMsg;
+}
+
+// Defining a function to validate form 
+function validateForm() {
+    // Retrieving the values of form elements 
+    var firstname = document.contactForm.firstname.value;
+    var lastname = document.contactForm.lastname.value;
+    var email = document.contactForm.email.value;
+    var password = document.contactForm.password.value;
+   
+   	// Defining error variables with a default value
+       var firstnameErr = lastnameErr = emailErr = true;
+    
+       // Validate name
+       if(firstname == "") {
+           printError("firstnameErr", "Please enter your first name");
+       }
+
+       if(lastname == "") {
+        printError("lastnameErr", "Please enter your last name");
+    }
+    if(password == "") {
+        printError("passwordErr", "Please enter your password");
+    }
+
+
+    // Validate email address
+    if(email == "") {
+        printError("emailErr", "Please enter your email address");
+    } else {
+        // Regular expression for basic email validation
+        var regex = /^\S+@\S+\.\S+$/;
+        if(regex.test(email) === false) {
+            printError("emailErr", "Please enter a valid email address");
+        } else{
+            printError("emailErr", "");
+            emailErr = false;
+        }
+    }
+}
