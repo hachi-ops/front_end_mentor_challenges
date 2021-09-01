383bfcdb3793b3b4e4d42db61d015071456758d5
hachi-ops <elen20@protonmail.com>
2021-09-01 20:47:32 +0100

    form validation: added error messages

--- script.js
+++ script.js
@@ -1,74 +1,66 @@
-
-
-// if (input.length < 1) {
-//     document.getElementsByClassName('error-text').style.display = 'block';
-// }
-
-// const form = document.getElementById('form');
-// const firstName = document.getElementById('fName');
-// const lastName = document.getElementById('lName');
-// const email = document.getElementById('email');
-// const password = document.getElementById('password');
-
-
-
-// function errorFunc(req, message){
-//     const formControl = req.parentElement;
-//     const span = formControl.querySelector('span');
-//     req.className += 'errors';
-//     span.className += 'errors-text';
-// }
-
-// function successFunc(req){
-//     req.className += 'success';
-// }
-
-// function myFunction() {
-//     document.querySelector(".error-text").innerHTML = "Hello World";
-//   }
-
-
-
-// Defining a function to display error message
-function printError(elemId, hintMsg) {
-    document.getElementById(elemId).innerHTML = hintMsg;
+// show a message with a type of the input
+function showMessage(input, message, type) {
+	// get the small element and set the message
+	const msg = input.parentNode.querySelector("small");
+	msg.innerText = message;
+	// update the class for the input
+	input.className = type ? "success" : "error";
+	return type;
 }
 
-// Defining a function to validate form 
-function validateForm() {
-    // Retrieving the values of form elements 
-    var firstname = document.contactForm.firstname.value;
-    var lastname = document.contactForm.lastname.value;
-    var email = document.contactForm.email.value;
-    var password = document.contactForm.password.value;
-   
-   	// Defining error variables with a default value
-       var firstnameErr = lastnameErr = emailErr = true;
-    
-       // Validate name
-       if(firstname == "") {
-           printError("firstnameErr", "Please enter your first name");
-       }
-
-       if(lastname == "") {
-        printError("lastnameErr", "Please enter your last name");
-    }
-    if(password == "") {
-        printError("passwordErr", "Please enter your password");
-    }
-
-
-    // Validate email address
-    if(email == "") {
-        printError("emailErr", "Please enter your email address");
-    } else {
-        // Regular expression for basic email validation
-        var regex = /^\S+@\S+\.\S+$/;
-        if(regex.test(email) === false) {
-            printError("emailErr", "Please enter a valid email address");
-        } else{
-            printError("emailErr", "");
-            emailErr = false;
-        }
-    }
+function showError(input, message) {
+	return showMessage(input, message, false);
 }
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+	// check if the value is not empty
+	if (!hasValue(input, requiredMsg)) {
+		return false;
+	}
+	// validate email format
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const email = input.value.trim();
+	if (!emailRegex.test(email)) {
+		return showError(input, invalidMsg);
+	}
+	return true;
+}
+
+
+const form = document.querySelector("#signup");
+
+const FIRSTNAME_REQUIRED = "Please enter your name";
+const LASTNAME_REQUIRED = "Please enter your name";
+const EMAIL_REQUIRED = "Please enter your email";
+const EMAIL_INVALID = "Please enter a correct email address format";
+const PASSWORD_REQUIRED = "Please enter password";
+
+
+form.addEventListener("submit", function (event) {
+	// stop form submission
+	event.preventDefault();
+
+	// validate the form
+	let firstnameValid = hasValue(form.elements["firstname"], FIRSTNAME_REQUIRED);
+    let lastnameValid = hasValue(form.elements["lastname"], FIRSTNAME_REQUIRED);
+	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+    let passwordValid = hasValue(form.elements["password"], PASSWORD_REQUIRED);
+	// if valid, submit the form.
+	if (firstnameValid && lastnameValid && emailValid && passwordValid) {
+		alert("Demo only. No form was posted.");
+	}
+});
